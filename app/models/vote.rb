9379958ea284b80ef603fc9055509b6db62e2c42
Ansell Gabriel Del Ayre <ansellgabrieldelayre@Ansells-MacBook-Pro.local>
2023-02-27 14:46:11 +0800

    update election validation and design

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,8 +9,12 @@ class Vote < ApplicationRecord
   def validate_vote 
     @count_vote = Vote.where(position_id: position_id, member: member_id).count
     @max_vote = Position.find(position_id).vacant
+    @event = Event.find(event_id)
     if @count_vote == @max_vote
       errors.add(:base,"You already voted " + @count_vote.to_s + " candidate.")
     end
+    # if @event.required_complete && @count_vote < @max_vote
+    #   errors.add(:base,"You have to vote " + @count_vote.to_s + " more candidate(s).")
+    # end
   end
 end
