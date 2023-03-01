1657f82d12a45ea8be8b6925d411739ded5e057c
Ansell Gabriel Del Ayre <ansellgabrieldelayre@Ansells-MacBook-Pro.local>
2023-03-01 17:21:53 +0800

    validations

--- app/models/election.rb
+++ app/models/election.rb
@@ -2,10 +2,11 @@ class Election < ApplicationRecord
   belongs_to :member, optional: true
   # validates :voter_code, presence :true
   validates_presence_of :voter_code
+  validate :check_votercode
 
   def check_votercode
-    @event = Event.findy_by(active: 1)
-    @member = Member.find_by(event: @event voter_code: voter_code)
+    @event = Event.find_by(active: 1)
+    @member = Member.find_by(event: @event, vote_code: voter_code)
     unless @event.election 
       errors.add(:base,"Election is now close")   
     end
@@ -13,7 +14,7 @@ class Election < ApplicationRecord
       errors.add(:base,"Voter code not valid")   
     else
       if @member.voted
-        errors.add(:base,"Cooperative already voted")
+        errors.add(:base,"Member already voted")
       end
     end
     
